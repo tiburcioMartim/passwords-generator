5d3527c51615c6aa3c7296d81548c059ef2e9929
Martim Tiburcio <tiburciomartim@gmail.com>
2024-11-18 09:08:16 -0300

    update

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,4 @@
-import random, string, csv, os
+import string, random, csv, os
 
 def gerar_senha(tamanho, usar_maiusculo, usar_numeros, usar_especiais):
     caracteres = string.ascii_lowercase
@@ -12,22 +12,38 @@ def gerar_senha(tamanho, usar_maiusculo, usar_numeros, usar_especiais):
     senha = "".join(random.choice(caracteres) for i in range(tamanho))
     return senha
 
-tamanho = int(input("Qual será o tamanho da senha? "))
-usar_maiusculo = input("Incluir letras maiusculas? (s/n)").lower() == "s"
-usar_numeros = input("Incluir números? (s/n)").lower() == "s"
-usar_especiais = input("Incluir caracteres especiais? (s/n)").lower() == "s"
+def gerar_log():
+    if os.path.exists("log.csv"):
+        with open("log.csv", "r", newline="") as csvfile:
+            reader = csv.reader(csvfile)
+            index = sum(1 for row in reader)
+    else:
+        index = 0
+    
+    with open("log.csv", "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([index, senha])
 
-senha = gerar_senha(tamanho, usar_maiusculo, usar_numeros, usar_especiais)
+def main():
+    while(input("Deseja criar uma senha? (s/n): ").lower() == "s"):
+    
+        tamanho = int(input("Qual será o tamanho da senha? "))
+        usar_maiusculo = input("Incluir letras maiusculas? (s/n)").lower() == "s"
+        usar_numeros = input("Incluir números? (s/n)").lower() == "s"
+        usar_especiais = input("Incluir caracteres especiais? (s/n)").lower() == "s"
 
-if os.path.exists("log.csv"):
-    with open("log.csv", "r", newline="") as csvfile:
-        reader = csv.reader(csvfile)
-        index = sum(1 for row in reader) + 1
-else:
-    index = 0
+        global senha
+        senha = gerar_senha(tamanho, usar_maiusculo, usar_numeros, usar_especiais)
+        largura_decoradora = 38
+        text = f"SENHA GERADA"
+        print(
+            f"""\n{"=" * largura_decoradora}
+            \n{text.center(largura_decoradora)}
+            \n{senha.center(largura_decoradora)}
+            \n{"=" * largura_decoradora}"""
+        )
+        
+        gerar_log()
 
-with open("log.csv", "a", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow([index, senha])
+main()
 
-print(f"Sua senha gerada é: {senha}")
